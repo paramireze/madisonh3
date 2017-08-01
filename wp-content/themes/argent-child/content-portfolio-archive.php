c687c6e464a6a62763b0f8c3702343ee6217cac2
paramireze <paramireze@gmail.com>
2017-07-31 21:07:26 -0500

    add portfolio archive page to child theme and edited it.

---
+++ wp-content/themes/argent-child/content-portfolio-archive.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The template used for displaying Portfolio Archive view
+ *
+ * @package Argent
+ */
+?>
+
+<header class="page-header">
+	<?php echo "<h2 class='page-title'>News & Announcements</h2>" ?>
+
+	<?php argent_portfolio_thumbnail( '<div class="portfolio-featured-image">', '</div>'); ?>
+
+	<?php argent_portfolio_content( '<div class="taxonomy-description">', '</div>' ); ?>
+</header><!-- .page-header -->
+<div id="portfolio-wrapper">
+    <h2 style="text-align:center;" class="section-title">Add News Item or Hashcap</h2>
+    <div>Have an announcement or a Hashcap you want to post? Well go fuck yourself. Then contact OTC at madjestic_stallion@yahoo.com with a copy of your post.</div>
+    <hr />
+    <?php /* Start the Loop */ ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', 'portfolio' ); ?>
+
+	<?php endwhile; ?>
+</div><!-- #portfolio-wrapper -->
+<?php
+	the_posts_navigation( array(
+		'prev_text'          => esc_html__( 'Older projects', 'argent' ),
+		'next_text'          => esc_html__( 'Newer projects', 'argent' ),
+		'screen_reader_text' => esc_html__( 'Portfolio navigation', 'argent' ),
+	) );
+?>
